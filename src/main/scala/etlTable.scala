7afb807c61016b75d37d38db4399b6e0b61c769d
k_wfh <wangfh00@163.com>
2022-08-12 16:50:42 +0800

    修改目录层次结构

--- src/main/scala/etlTable.scala
+++
@@ -1,8 +0,0 @@
-import org.apache.spark.sql.SparkSession
-
-object etlTable {
-  def main(args: Array[String]): Unit = {
-    val session = SparkSession.builder().appName("etlTable").master("local[*]").getOrCreate()
-
-  }
-}
